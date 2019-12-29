929259310e618ead19f1b4d5dd672abe04b2bb3d
MariaBlesstinasagayanathan <59302300+MariaBlesstinasagayanathan@users.noreply.github.com>
2019-12-29 22:29:14 +0530

    Add files via upload

---
+++ mewpassword.java
@@ -0,0 +1,72 @@
+package newpwd;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class mewpassword {
+	WebDriver driver;
+  @Test
+  public void f() {
+	  driver.findElement(By.id("cyclosUsername")).sendKeys("admin");
+	  driver.findElement(By.id("cyclosPassword")).sendKeys("123456");
+	  driver.findElement(By.xpath("//input[@value='1']")).click();
+
+	  driver.findElement(By.xpath("//input[@value='2']")).click();
+	  driver.findElement(By.xpath("//input[@value='3']")).click();
+	  driver.findElement(By.xpath("//input[@value='4']")).click();
+	  driver.findElement(By.xpath("//input[@value='5']")).click();
+	  driver.findElement(By.xpath("//input[@value='6']")).click();
+	  driver.findElement(By.xpath("//input[@value='Submit']")).click();
+
+	  driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/ul/li[2]/span[2]")).click();
+	  driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/ul/li[2]/ul/li[2]/span[2]")).click();
+	  driver.findElement(By.name("oldPassword")).sendKeys("123456");
+	  driver.findElement(By.name("newPassword")).sendKeys("1234");
+	  driver.findElement(By.name("newPasswordConfirmation")).sendKeys("1234");
+	  driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[3]/form/table/tbody/tr[2]/td/table/tbody/tr[5]/td/input")).click();
+	  String expresult = "Password are Equal message in pop up window should get displayed";
+	  Alert alert = driver.switchTo().alert();
+	 String actresult = alert.getText();
+	 System.out.println(actresult);
+	 Assert.assertTrue(actresult.contains(expresult));
+
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  driver.get("http://localhost:8585/");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+	  driver = new ChromeDriver();
+
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
